1087f64a19f1618c6f1310e7462d0a4c69790d89
hadysoufan <hadisoufan74@gmail.com>
2023-09-02 16:49:46 +0300

    Editing

---
+++ SIS_UMS/SIS_UMS/Controllers/AccountingController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SIS_UMS.Controllers
+{
+    [Route("accounting")]
+    public class AccountingController : Controller
+    {
+        [HttpGet("accounting")]
+        public IActionResult Accounting()
+        {
+            return View();
+        }
+    }
+}
